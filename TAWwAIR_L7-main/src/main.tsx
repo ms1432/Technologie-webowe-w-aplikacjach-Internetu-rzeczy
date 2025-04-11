5abb034639f33eddf76865010fd491b4883acccb
Michał <mstrek1432@gmail.com>
2025-04-11 16:29:07 +0200

    „Lab07”

--- TAWwAIR_L7-main/src/main.tsx
+++ TAWwAIR_L7-main/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Chart from './assets/Chart.tsx'
+import Chart from './Chart.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
